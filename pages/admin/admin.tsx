29dfe8725550f2fa2758e1c6d54f3cd2496bad9a
ThanosDodd <36003580+ThanosDodd@users.noreply.github.com>
2022-06-19 13:08:22 +0800

    admin page not in index

---
+++ pages/admin/admin.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import { useSession } from "next-auth/react";
+
+import styles from "../../styles/Home.module.css";
+
+const AdminPage: NextPage = () => {
+  //NextAuth
+  const { data: session } = useSession();
+
+  //API fetcher
+  const fetcher = () =>
+    fetch("/api/adminClose", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ userName: session?.user?.name }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        alert(json.results);
+      });
+
+  return (
+    <>
+      <Head>
+        <title>Better First Contributions Admin</title>
+        <meta
+          name="description"
+          content="Learn how to contribute to Open Source without the fear of failure"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {session?.user?.name === "ThanosDodd" ? (
+        <button className={styles.buttonClass} onClick={() => fetcher()}>
+          Do the stuff
+        </button>
+      ) : (
+        <h1>You are not an admin</h1>
+      )}
+    </>
+  );
+};
+
+export default AdminPage;
