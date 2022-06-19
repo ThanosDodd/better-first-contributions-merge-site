a62d755085c59f0510d462a95db3acf77d7ff200
ThanosDodd <36003580+ThanosDodd@users.noreply.github.com>
2022-06-19 20:25:37 +0800

    images

---
+++ pages/assets/index.tsx
@@ -0,0 +1,12 @@
+function Pics() {
+  return (
+    <>
+      <img src="/fork.png" alt="" />
+      <img src="/clone.png" alt="" />
+      <img src="/compPull.png" alt="" />
+      <img src="/createPR.png" alt="" />
+    </>
+  );
+}
+
+export default Pics;
